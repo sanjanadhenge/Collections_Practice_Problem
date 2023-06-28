724326d21abf221fe11fda36a8ae76b4648cd936
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-28 15:48:55 +0530

    [sanjana] Add : Collection Practice Problems-Count of remaining chapter

---
+++ Collections/CountOfRemainingChapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class CountOfRemainingChapter
+    {
+        public int CalaculateCount(int N,Dictionary<int,int> dict,int K)
+        {
+            int count = 0;
+            foreach(var item in dict)
+            {
+                if (item.Value>=K)
+                {
+                    count++;
+                }
+          
+            }
+            return count;
+           
+        }
+    }
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Total Number of Chapter");
+            int N= Convert.ToInt32(Console.ReadLine());
+            Dictionary<int, int> dict = new Dictionary<int, int>(N);
+            dict.Add(1, 4);
+            dict.Add(5, 7);
+            dict.Add(9, 16);
+            dict.Add(17, 26);
+            Console.WriteLine("Enter current reading page");
+            int K = Convert.ToInt32(Console.ReadLine());
+            CountOfRemainingChapter remainingChapter = new CountOfRemainingChapter();
+           int result = remainingChapter.CalaculateCount(N, dict, K);
+            Console.WriteLine("Remaining Chapter count is : " + result);
+        }
+        
+    }
+}
