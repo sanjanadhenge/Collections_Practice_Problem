d08f68aad42f82849369925db5a743a54f411ef5
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-28 17:37:04 +0530

    [sanjana] Add : Maximum Selling Price

---
+++ Collections/MaximumSellPrice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class MaximumSellPrice
+    {
+        public void FindMaxPrice(int K,List<int> A, List<int> B)
+        {
+            int temp = 0;
+            A.Sort();
+            B.Sort();
+            int c = B.Count;
+            for(int i=0;i<K;i++)
+            {
+                A[i] = B[c-1];
+                c--;
+            }
+
+            int sum1=0,sum2=0;
+             foreach(var item in A)
+            {
+               sum1 += item;
+            }
+            foreach (var item in B)
+            {
+
+                sum2 += item;
+            }
+            if(sum1 > sum2)
+            {
+                Console.WriteLine(sum1);
+            }
+            else
+            {
+                Console.WriteLine(sum2);
+            }
+        }
+
+    }
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -12,19 +12,35 @@ namespace Collections
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Total Number of Chapter");
-            int N= Convert.ToInt32(Console.ReadLine());
-            Dictionary<int, int> dict = new Dictionary<int, int>(N);
-            dict.Add(1, 4);
-            dict.Add(5, 7);
-            dict.Add(9, 16);
-            dict.Add(17, 26);
+            // Console.WriteLine("Enter the Total Number of Chapter");
+            // int N= Convert.ToInt32(Console.ReadLine());
+            // Dictionary<int, int> dict = new Dictionary<int, int>(N);
+            // dict.Add(1, 4);
+            // dict.Add(5, 7);
+            // dict.Add(9, 16);
+            // dict.Add(17, 26);
+            // Console.WriteLine("Enter current reading page");
+            // int K = Convert.ToInt32(Console.ReadLine());
+            // CountOfRemainingChapter remainingChapter = new CountOfRemainingChapter();
+            //int result = remainingChapter.CalaculateCount(N, dict, K);
+            // Console.WriteLine("Remaining Chapter count is : " + result);
+            Console.WriteLine("Enter the fruits in bucket");
+            int N = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter current reading page");
             int K = Convert.ToInt32(Console.ReadLine());
-            CountOfRemainingChapter remainingChapter = new CountOfRemainingChapter();
-           int result = remainingChapter.CalaculateCount(N, dict, K);
-            Console.WriteLine("Remaining Chapter count is : " + result);
+            List<int> A = new List<int>(N);
+            List<int> B = new List<int>(N);
+            A.Add(2);
+            A.Add(3);
+            A.Add(2);
+            A.Add(4);
+            B.Add(2);
+            B.Add(4);
+            B.Add(2);
+            B.Add(3);
+            MaximumSellPrice maximumSellPrice = new MaximumSellPrice();
+            maximumSellPrice.FindMaxPrice(K,A,B);
         }
-        
+
     }
 }
